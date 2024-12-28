9228bd2ca7e566c3b69eafc8ce31246f6e6c54ed
Md. Nesad <131846011+Md-Nesad@users.noreply.github.com>
2024-12-28 23:51:53 +0600

    Next js practice

---
+++ app/check/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Check() {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+        nostrum nisi quibusdam quos amet fugiat. Ab laborum eius perferendis sit
+        cupiditate consectetur, itaque modi facere numquam explicabo
+        perspiciatis quo culpa.
+      </h1>
+    </div>
+  );
+}
